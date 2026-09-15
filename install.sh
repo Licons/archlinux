0fa65c03984df9df3ca7cbb1691bc1fadf5537cf
Qua, Chau Ngoc <quacn@utop.io>
2026-09-15 17:21:06 +0700

    update

--- install.sh
+++ install.sh
@@ -226,7 +226,7 @@ EOF
     c|cinnamon)
         pacman -S --noconfirm --needed \
             xorg-server xorg-xwayland \
-            cinnamon \
+            cinnamon udisks2 gvfs gvfs-mtp libmtp \
             lightdm lightdm-gtk-greeter lightdm-webkit2-greeter \
             gnome-terminal gnome-screenshot gnome-system-monitor gnome-calculator gnome-calendar gnome-characters \
             nemo-fileroller nemo-terminal nemo-share \
