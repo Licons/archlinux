9e32f58d608c4c1d1bba4949c1472f315b1f9de3
Licons Chou <liconschou@gmail.com>
2026-09-15 22:45:42 +0700

    update

--- config.sh
+++ config.sh
@@ -63,7 +63,7 @@ if status is-interactive
 end
 
 set -g fish_greeting
-set -gx PATH $PATH $HOME/.dotnet/tools $HOME/.local/bin
+set -gx PATH /usr/local/bin \$HOME/.dotnet/tools \$HOME/.local/bin
 set -Ux SSL_CERT_DIR "$HOME/.aspnet/dev-certs/trust:/etc/ssl/certs"
 set -x DOTNET_CLI_TELEMETRY_OPTOUT 1
 set -x LC_ALL C.UTF-8
