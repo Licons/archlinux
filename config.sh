3ac3046ee32b395b638d19dc5b54a778cea89529
Licons Chou <liconschou@gmail.com>
2026-09-15 22:45:57 +0700

    update

--- config.sh
+++ config.sh
@@ -64,7 +64,7 @@ end
 
 set -g fish_greeting
 set -gx PATH /usr/local/bin \$HOME/.dotnet/tools \$HOME/.local/bin
-set -Ux SSL_CERT_DIR "$HOME/.aspnet/dev-certs/trust:/etc/ssl/certs"
+set -Ux SSL_CERT_DIR "\$HOME/.aspnet/dev-certs/trust:/etc/ssl/certs"
 set -x DOTNET_CLI_TELEMETRY_OPTOUT 1
 set -x LC_ALL C.UTF-8
 EOF
